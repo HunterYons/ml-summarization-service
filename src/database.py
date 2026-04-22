b91eeab4194f405b67fc6ad2aca9441973309c3e
Yons <andrey-1182@mail.ru>
2026-04-22 11:15:15 +0300

    Изменение кода

--- src/database.py
+++ src/database.py
@@ -1,5 +1,5 @@
 import os
-from sqlalchemy import create_all, create_engine, Column, Integer, String, Text, DateTime
+from sqlalchemy import create_engine, Column, Integer, String, Text, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import datetime
